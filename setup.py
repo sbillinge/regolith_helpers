ad280dcebb4296478d3985e7e27e0ab340999e74
Simon Billinge <simon.billinge@gmail.com>
2020-04-09 13:20:09 -0400

    renaming package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 
 setuptools.setup(
-    name="regolith-helpers",   # Replace with your own name
+    name="regolith_helpers",   # Replace with your own name
     version="0.0.1",
     author="example author",
     author_email="sb2896@columbia.edu",
@@ -15,15 +15,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/billingegroup/regolith-helpers",
     packages=setuptools.find_packages(),
-    package_dir={"regolith-helpers": "regolith-helpers"},
+    package_dir={"regolith_helpers": "regolith-helpers"},
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ],
-    entry_points={'console_scripts': [
-            'regolith-helpers = regolith-helpers.main:main',],
-        },
+#    entry_points={'console_scripts': [
+#            'regolith-helpers = regolith-helpers.main:main',],
+#        },
     data_files = [("", ["LICENSE.txt"])],
     python_requires='>=3.7',
     zip_safe=False,
